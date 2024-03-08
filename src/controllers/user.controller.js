6e78c341cf5816d628ee15355609b533d6356a97
SudhirYadav <sudhirkyadav51@gmail.com>
2024-03-08 22:21:39 +0530

    aoi request in postman generated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utlites/asyncHandler.js";
 
 const registerUser= asyncHandler(async(req, res)=>{
     res.status(200).json({
-        message:"OK"
+        message:"Sudhir yadav"
     })
 })
 
--- src/index.js
+++ src/index.js
@@ -10,15 +10,14 @@ dotenv.config({
 
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>{
-        console.log('Server is running at PORT ${process.env.PORT}');
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
     })
 })
-.catch((err)=>{
-    console.log("MongoDB failed", err);
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
-
 
 
 
